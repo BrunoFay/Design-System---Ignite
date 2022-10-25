69537eaaaadcc29324cb9e4fc969d7245f939929
bruno <brunofay1@hotmail.com>
2022-10-25 16:06:00 -0300

    NEW add components to storiebook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,34 +1,9 @@
-import { colors } from '@bfay/tokens'
-import { ComponentProps } from 'react'
-import { styled } from './styles'
-console.log(colors)
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$sm',
-  padding: '$4',
-  '&:hover': {
-    backgroundColor: '$ignite300',
-  },
-  color: '$white',
-  border: 'none',
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: '$2 $4',
-      },
-      big: {
-        fontSize: 16,
-        padding: '$3 $6',
-      },
-    },
-  },
-  defaultVariants: {
-    size: 'small',
-  },
-})
-
-/* forma para pegar a tipagem dos atributos do component */
-export type ButtonProps = ComponentProps<typeof Button>
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/Avatar'
+export * from './components/Button'
+export * from './components/TextInput'
+export * from './components/TextArea'
+export * from './components/Checkbox'
+export * from './components/MultiStep'
